20552a6dd77c060d75987ff184add6048b17a61f
rohamafatimajee-eng <rohamafatimajee@gmail.com>
2026-03-22 16:52:42 +0500

    add

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+include("db.php");
+
+if ($_POST) {
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+
+  $result = $conn->query("SELECT * FROM users WHERE email='$email'");
+  $user = $result->fetch_assoc();
+
+  if ($user && password_verify($password, $user['password'])) {
+    $_SESSION['user_id'] = $user['id'];
+    header("Location: dashboard.php");
+  } else {
+    echo "Invalid login";
+  }
+}
+?>
+
+<form method="POST">
+<h2>Login</h2>
+<input type="email" name="email" placeholder="Email" required>
+<input type="password" name="password" placeholder="Password" required>
+<button>Login</button>
+</form>
+
+<a href="signup.php">Signup</a>
