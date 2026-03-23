61086cd61278e828d9863c6a1bbb18308c6a7ffa
rohamafatimajee-eng <rohamafatimajee@gmail.com>
2026-03-23 17:37:41 +0500

    new

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "if0_41259647", "NHt3lTbzW5WOA", "if0_41259647_calorietracker");
+$conn = new mysqli("sql106.infinityfree.com", "if0_41259647", "NHt3lTbzW5WOA", "if0_41259647_calorietracker");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
